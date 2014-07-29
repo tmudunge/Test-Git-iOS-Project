43003a8e4028b11fc3a469fa0050ec1d78c03522
tmudunge <tendaimudunge@h74-3-178-150.bigpipeinc.com>
2014-07-28 22:36:11 -0600

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Tendai Mudunge on 2014-07-28.
+//  Copyright (c) 2014 ___Tendai Mudunge___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Tendai Mudunge on 2014-07-28.
+//  Copyright (c) 2014 ___Tendai Mudunge___. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
